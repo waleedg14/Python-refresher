24621bed901281a9151f7f1fa9720450af296993
Waleed <waleedghaleb@gmail.com>
2025-10-19 15:37:41 -0700

    the code

---
+++ 16_func_arguments/code.py
@@ -0,0 +1,14 @@
+# arguments
+def add(x,y):
+    result = x+y
+    print(result)
+
+# paramters
+add(5,3)
+
+# ---------------------------------
+
+def say_hello():
+    print(f"Hello!, {name}")
+
+say_hello("Bob")
